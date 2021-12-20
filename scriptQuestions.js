54e6d09ddd98c67a0e4c41db1cf2946d4e84305c
Svyryda Yevhenii <timkshuk@gmail.com>
2021-12-20 22:27:50 +0200

    create

---
+++ scriptQuestions.js
@@ -0,0 +1,14 @@
+let modelPopup = document.querySelector(".button__footer");
+let close = document.querySelector();
+
+
+
+document.querySelector(".button__footer--create").disabled = true;
+let input = document.getElementById(".button__footer--create");
+if (input && input.value) {
+    //выполнить отправку
+}
+else{
+    //показать сообщение (множество вариантов)
+    alert('Ошибка при отправке данных, заполните поле');
+}
