d27e53d091fd55f643370a30f7eec62d075425ed
Olha Starosta <shop2029@gmail.com>
2021-12-25 21:19:18 +0200

    home page

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let developersArr = null;
 
+const avatarInput = document.getElementById('edit-avatar')
 const nameInput = document.getElementById('edit-name')
 const ageInput = document.getElementById('edit-age')
 const idInput = document.getElementById('edit-id')
+const genderInput = document.getElementById('edit-gender')
+const cityInput = document.getElementById('edit-city')
+const activityInput = document.getElementById('edit-activity')
+const companyInput = document.getElementById('edit-company')
+const interestsInput = document.getElementById('edit-interests')
 const form = document.getElementById('edit-dev');
+
 
 function loadData() {
   fetch('/developers')
@@ -25,7 +32,9 @@ loadData();
 
 function renderCards(data) {
   const cards = data.map((dev) => {
-    return `<span class="name">${dev.name}</span> <b class="age">${dev.age}</b><button onclick="editData(${dev.id})">edit</button><br>`
+    return `<img src="${dev.avatar}" class="avatar"><span class="name">${dev.name}</span><b class="age">${dev.age}</b><span class="gender">${dev.gender}</span>
+<span class="city">${dev.city}</span><span class="activity">${dev.activity}</span><span class="company">${dev.company}</span>
+<span class="interests">${dev.interests}</span><button onclick="editData(${dev.id})">edit</button><br>`
   }).join('');
 
   developers.innerHTML = cards;
@@ -34,8 +43,14 @@ function renderCards(data) {
 function editData(id) {
   const dev = developersArr[id];
 
+  avatarInput.value = dev.avatar;
   nameInput.value = dev.name;
   ageInput.value = dev.age;
+  genderInput.value = dev.gender;
+  cityInput.value = dev.city;
+  activityInput.value = dev.activity;
+  companyInput.value = dev.company;
+  interestsInput.value = dev.interests;
   idInput.value = dev.id;
   form.style.display = 'block';
 }
@@ -45,8 +60,14 @@ form.addEventListener('submit', (e) => {
 
   const updatedData = {
     id: +idInput.value,
+    avatar: avatarInput.value,
     name: nameInput.value,
-    age: +ageInput.value
+    age: +ageInput.value,
+    gender: genderInput.value,
+    city: cityInput.value,
+    activity: activityInput.value,
+    company: companyInput.value,
+    interests: interestsInput.value
   }
 
   fetch('/developers', {
@@ -57,8 +78,13 @@ form.addEventListener('submit', (e) => {
     body: JSON.stringify(updatedData),
   }).then(() => {
     loadData();
-    nameInput.value = ''
+    avatarInput.value = '';
+    nameInput.value = '';
     ageInput.value = '';
+    genderInput.value = '';
+    cityInput.value = '';
+    activityInput.value = '';
+    interestsInput.value = '';
     idInput.value = '';
     form.style.display = 'none';
   });
