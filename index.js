9391fbb147f297a2f0424892f786b22c1a568dcd
Alexander <alexanderpolyanitsa94@gmail.com>
2021-12-25 16:36:52 +0200

    Fix-server

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+
+
+const urlencodedParser = express.urlencoded({extended: false});
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// HTML, JS
+
+app.get('/', (req, res) => {
+  const index = fs.readFileSync('./src/indexHome.html', 'utf8');
+  res.send(index);
+});
+
+app.get('/script.js', (req, res) => {
+  const index = fs.readFileSync('./script.js', 'utf8');
+  res.send(index);
+});
+
+
+app.use(express.static("src"));
+
+
+// Data
+app.get('/developers', (req, res) => {
+  const devsFile = JSON.parse(fs.readFileSync('./developers.json', 'utf8'));
+
+  res.set({
+    "Content-Type": "application/json",
+  });
+  
+  res.json(devsFile)
+})
+
+app.post('/developers', urlencodedParser, (req, res) => {
+  if(!req.body) return res.sendStatus(400);
+
+  const { id, name, age } = req.body;
+  
+  const developers = JSON.parse(fs.readFileSync('./developers.json', 'utf8'));
+
+  developers[id] = {
+    id, name, age,
+  }
+
+  fs.writeFileSync('./developers.json', JSON.stringify(developers), 'utf8');
+
+  res.send(400);
+})
+
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:3000`)
+})
