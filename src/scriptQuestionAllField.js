0a297b7f97b2f47e6f9cedbf17c84629cf2cdd5b
Olha Starosta <shop2029@gmail.com>
2022-01-16 13:54:23 +0200

    add local storage

--- src/scriptQuestionAllField.js
+++ src/scriptQuestionAllField.js
@@ -8,9 +8,8 @@ const questionData = {
 const questionField = document.querySelector('.pop_up_question'); //поле ввода текста
 const choiceOfTheme = document.querySelector('.listSelector'); // выпадашка выбора тем
 const answer = document.querySelectorAll('input[type=radio]'); // кнопки тру фолз
-const date = document.querySelector('date');
-const fileSystem = document.querySelectorAll('input[type=checkbox]'); // выбор файловой системы
-
+//const date = document.querySelector('date');
+const fileSystemArr = document.querySelectorAll('input[type=checkbox]'); // выбор файловой системы
 const btnCancel = document.querySelector('.btn--cancel');
 const btnCreate = document.querySelector('.btn--create'); // кнопка создания темы
 const selectFS = document.getElementById('selectorFile');
@@ -21,18 +20,18 @@ selectFS.addEventListener('change', (el) => {
     loadData(value);
 })
 
-function loadData(fileSistem) {
+function loadData(fileSystem) {
 
-    fetch(`/question${fileSistem}`)
+    fetch(`/question${fileSystem}`)
         .then((data) => {
             return data.json()
         })
 
         .then((data) => {
             console.log(data)
-            questionData[fileSistem] = data;
+            questionData[fileSystem] = data;
            // console.log(data)
-            renderCards(data, fileSistem);
+            renderCards(data, fileSystem);
         })
         .catch((e) => {
             console.log(e);
@@ -40,6 +39,8 @@ function loadData(fileSistem) {
 }
 
 loadData('JSON');
+loadData(getLocalStorage('selectFileSystem'));
+
 
 
 /* (если число меньше десяти, перед числом добавляем ноль) */
@@ -67,7 +68,7 @@ function date_time()
 }
 
 
-function renderCards(data, fileSistem) {
+function renderCards(data, fileSystem) {
     const cards = data.map((quest) => {
          return `<div class="questions">
 
@@ -75,7 +76,7 @@ function renderCards(data, fileSistem) {
                     <span class="theme">${quest.theme}</span>
                     <span class="answer">${quest.answer}</span>
                     <span class="date">${quest.date}</span>
-                    <button onclick="deleteQuestion(${quest.id}, '${fileSistem}')">DELETE</button>
+                    <button onclick="deleteQuestion(${quest.id}, '${fileSystem}')">DELETE</button>
                      
                     <br>
                </div>`
@@ -83,22 +84,22 @@ function renderCards(data, fileSistem) {
     list.innerHTML = cards;
 }
 
-function deleteQuestion(id, fileSistem) {
-    const indexId = questionData[fileSistem].findIndex((obj) => {
+function deleteQuestion(id, fileSystem) {
+    const indexId = questionData[fileSystem].findIndex((obj) => {
         return id === obj.id;
 
     })
-    questionData[fileSistem].splice(indexId, 1); // начиная с позиции 1, удалить 1 элемент
-    //console.log(questionData, fileSistem, indexId)
+    questionData[fileSystem].splice(indexId, 1); // начиная с позиции 1, удалить 1 элемент
+    //console.log(questionData, fileSystem, indexId)
 
-    fetch(`/question${fileSistem}`, {
+    fetch(`/question${fileSystem}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(questionData[fileSistem]),
+        body: JSON.stringify(questionData[fileSystem]),
     }).then(() => {
-        loadData(fileSistem);
+        loadData(fileSystem);
 
     });
 }
@@ -107,7 +108,7 @@ btnCreate.addEventListener('click', (e) => {
     e.preventDefault();
     let findInput =[...answer].find((input)=> input.checked)
 let addFSArr = []
-    let findFS = [...fileSystem].find((input) => {
+    let findFS = [...fileSystemArr].find((input) => {
         if (input.checked) {
          let addSistem = input.value;
             addFSArr.push(addSistem)
